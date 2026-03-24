72cb456bb11bc90c043c844d6c16b17f94840bcd
paras madan <parasmadan3105@gmail.com>
2026-03-24 13:53:58 +0530

    home page completed

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -10,7 +10,7 @@ const ChatContainer = ({ selectedUser ,setSelectedUser}) => {
     if(scrollEnd.current){
       scrollEnd.current.scrollIntoView({behavior:'smooth'})
     }
-  }, [])
+  }, [selectedUser])
 
   return selectedUser ? ( 
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
@@ -26,8 +26,8 @@ const ChatContainer = ({ selectedUser ,setSelectedUser}) => {
         <img src={assets.help_icon} alt="" className='max:md:hidden max-w-5'/>
       </div>
       {/* chat area */}
-      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
-        {messagesDummyData.map((msg,index) => 
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3'>
+        {messagesDummyData.map((msg,index) =>(
           <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
             {msg.image ? (
               <img src = {msg.image} alt = "" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
@@ -42,19 +42,27 @@ const ChatContainer = ({ selectedUser ,setSelectedUser}) => {
               <p className='text-gray-500'>
                 {formatMessageTime(msg.createdAt)}
               </p>
-
-            </div>
-            
+            </div>  
           </div>
         
-        )} 
+        ))} 
+        <div ref ={scrollEnd}></div>
+      </div>
+      
+      {/* bottom area */}
 
-
-        <div ref ={scrollEnd}>
-
+      <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+        <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+          <input type="text" placeholder='Send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+          <input type="file"  id = "image" accept="image/png, image/jpeg" hidden/>
+          <label htmlFor="image">
+            <img src={assets.gallery_icon} alt=""  className='w-5 mr-2 cursor-pointer'/>
+          </label>
         </div>
 
+      <img src= {assets.send_button} alt="" className='w-7 cursor-pointer' />
       </div>
+
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
